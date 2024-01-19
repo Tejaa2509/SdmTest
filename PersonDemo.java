2a68c311eec7f768d2478ad44ac6401e6cf59907
Tejaa2509 <154808587+Tejaa2509@users.noreply.github.com>
2024-01-19 12:27:47 +0530

    Java File Added

---
+++ PersonDemo.java
@@ -0,0 +1,12 @@
+package Default;
+
+public class PersonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p=new Person();
+		p.Display();
+
+	}
+
+}
